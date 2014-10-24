66f4cac3b6f01f1bfee14fb99f0d8bf460555e69
Daniel Speck <daniel@dspeck.de>
2014-10-24 19:08:54 +0200

    Aufgabe 2.2 fertig

--- solutions/solution1/solution1.rkt
+++ solutions/solution1/solution1.rkt
@@ -54,7 +54,7 @@
   )
 
 ;;; Distanzen ausgeben
-(displayln "--- Aufgabe 2 ---")
+(displayln "--- Aufgabe 2.1 ---")
 
 (displayln "\n")
 
@@ -70,8 +70,49 @@
 
 (displayln "Osterinsel nach Lima")
 (distanzAB -27.10 -109.40 -12.10 -77.05)
+
+
+;; Aufgabe 2.2
+  
+; berechnet den unnormierten Anfangskurs in Gradmaß, Parameter werden in Gradmaß übergeben
+(define (AnfangskursUnnormiert bgradA lgradA bgradB lgradB)
+ (rad2deg
+  (acos
+  (/
+   (- 
+    (sin (deg2rad bgradB))
+    (* (cos (GKE bgradA lgradA bgradB lgradB)) (sin (deg2rad bgradA)))
+    )
+   (* (cos (deg2rad bgradA)) (sin (GKE bgradA lgradA bgradB lgradB)))
+   )
+  )
+  )
+)
+
+(define (Anfangskurs bgradA lgradA bgradB lgradB richtung)
+  (if (equal? richtung 'w)
+      (- 360 (AnfangskursUnnormiert bgradA lgradA bgradB lgradB))
+      (AnfangskursUnnormiert bgradA lgradA bgradB lgradB)
+   )
+  )
   
   
-  
-  
-  
+;;; Anfangskurs ausgeben
+(displayln "\n")
+
+(displayln "--- Aufgabe 2.2 ---")
+
+(displayln "\n")
+
+(displayln "Oslo nach Hongkong:")
+(Anfangskurs 59.93 10.75 22.20 114.10 'o)
+
+(displayln "\n")
+
+(displayln "San Francisco nach Honolulu:")
+(Anfangskurs 37.75 -122.45 21.32 -157.83 'w)
+
+(displayln "\n")
+
+(displayln "Osterinsel nach Lima")
+(Anfangskurs -27.10 -109.40 -12.10 -77.05 'o)
